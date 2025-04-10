27632bc33f496f19355931114f1ac7db69dc6ba4
leonardordss <lrodrigues.md@gmail.com>
2025-04-09 22:09:35 -0300

    Criando tela login e tela 2

--- projet_mahesh/lib/main.dart
+++ projet_mahesh/lib/main.dart
@@ -1,125 +1,129 @@
+import 'package:projet_mahesh/tela2.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Login(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Login extends StatefulWidget {
+  const Login({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Login> createState() => _LoginState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _LoginState extends State<Login> {
+  // Cria variáveis para armazenar o usuário e a senha
+  TextEditingController user = TextEditingController();
+  TextEditingController senha = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  // Função para verificar o login
+  _verificalogin() {
+    if (user.text == "Senai" && senha.text == "2025") {
+      // Chama a segunda tela
+      Navigator.push(context, MaterialPageRoute(builder: (context) => Tela2()));
+    } else {
+      // Exibe uma mensagem de erro caso o login seja incorreto
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Login incorreto"),
+          duration: Duration(seconds: 5),
+        ),
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      backgroundColor: const Color(0xFF86a880), // Cor de fundo da tela
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: SingleChildScrollView( // Adiciona um scroll caso o teclado apareça
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Container para a logo
+              Container(
+                width: 200,
+                height: 200,
+                color: const Color(0xFFFFFCF7),
+                child: Image.asset('assets/image/logo.jpeg', fit: BoxFit.fill),
+              ),
+              
+              SizedBox(height: 20),
+
+              // Container para os campos de login
+              Container(
+                padding: EdgeInsets.all(16), // Espaçamento interno
+                decoration: BoxDecoration(
+                  color: const Color(0xFFFFFCF7), // Cor de fundo do Container
+                  borderRadius: BorderRadius.circular(12), // Bordas arredondadas
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black,
+                      blurRadius: 8,
+                      offset: Offset(1, 1), // Sombra suave
+                    ),
+                  ],
+                ),
+                width: 250, // Largura do Container
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    // Campo de texto para o usuário
+                    TextField(
+                      keyboardType: TextInputType.name,
+                      decoration: InputDecoration(
+                        labelText: "Digite seu usuário",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      controller: user,
+                    ),
+                    SizedBox(height: 20), // Espaçamento entre os campos
+                    // Campo de texto para a senha
+                    TextField(
+                      keyboardType: TextInputType.number,
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      maxLength: 4, // Limita o número de caracteres
+                      decoration: InputDecoration(
+                        labelText: "Digite sua senha",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      obscureText: true,
+                      obscuringCharacter: '*',
+                      controller: senha,
+                      onSubmitted: (value) => _verificalogin(),
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(height: 20), // Espaçamento entre o Container e o botão
+              
+              // Botão de login
+              ElevatedButton(
+                onPressed: _verificalogin,
+                child: Text("Login"),
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 12),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ projet_mahesh/lib/tela2.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+
+class Tela2 extends StatelessWidget {
+  const Tela2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200], // Fundo bege (mais terroso)
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: const Color(0xFF86a880), // Verde Escuro
+              ),
+              child: Text('Menu', style: TextStyle(color: Colors.white)),
+            ),
+            ListTile(title: Text('Opção 1')),
+            ListTile(title: Text('Opção 2')),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF86a880), // Verde Escuro
+        elevation: 5,
+        iconTheme: IconThemeData(color: Colors.white), // Amarelo
+        centerTitle: true,
+        title: Row(
+          children: [
+            Image.asset('assets/image/logo.jpeg', height: 40),
+            Padding(
+              padding: EdgeInsets.all(12),
+              child: Text(
+                'App Mahesh Tech',
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w800,
+                    color: Colors.white),
+              ),
+            ),
+            Spacer(),
+            CircleAvatar(
+              backgroundColor: Colors.brown[100], // Marrom claro
+              child: Icon(Icons.person, color: const Color(0xFF86a880)), // Verde Escuro
+            )
+          ],
+        ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(12),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Buscar ...',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 30),
+          _DashboardButton(
+
+              icon: Icons.analytics_outlined,
+              label: 'Monitoramento',
+              onTap: () {}),
+          SizedBox(height: 30),
+          _DashboardButton(
+              icon: Icons.settings_remote_outlined,
+              label: 'Sistema de controle',
+              onTap: () {}),
+          SizedBox(height: 30),
+          _DashboardButton(
+              icon: Icons.smart_toy_outlined, label: 'Chatbot', onTap: () {}),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        selectedItemColor: Colors.white, // Amarelo
+        backgroundColor: const Color(0xFF86a880), // Verde Escuro
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: '',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _DashboardButton extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final VoidCallback onTap;
+
+  const _DashboardButton(
+      {required this.icon, required this.label, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+    
+      onTap: onTap,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        elevation: 5,
+        color: Colors.green[100],
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 12),
+          child: Row(
+            children: [
+              Icon(
+                icon,
+                color: Colors.brown,
+                size: 40,
+              ),
+              SizedBox(width: 16),
+              Text(
+                label,
+                style: TextStyle(fontSize: 18, color: Colors.brown), // Amarelo
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
